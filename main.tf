cbaf0a69ac453325ea51ddd180e04da36a96da32
Eero Ruohola <eero@ruohola.dev>
2020-11-16 20:08:07 +0200

    Use t2.micro again for QA instance
    Should be enough now that the memory leak got fixed.

--- main.tf
+++ main.tf
@@ -367,7 +367,7 @@ resource "aws_launch_configuration" "prod" {
 resource "aws_launch_configuration" "staging" {
   name_prefix          = "skole-staging-lc"
   image_id             = "ami-09509e8f8dea8ab83"
-  instance_type        = "t2.small"
+  instance_type        = "t2.micro"
   user_data            = "#!/bin/bash\necho ECS_CLUSTER=skole-staging-cluster >> /etc/ecs/ecs.config"
   key_name             = "skole"
   security_groups      = [aws_security_group.sg.id]
