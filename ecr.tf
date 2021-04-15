80ced9277e73ae95c91af5888d2c0254ab23c709
Eero Ruohola <eero@skoleapp.com>
2021-04-15 18:45:10 +0300

    Make `ecr_policy_keep_10` variable into a local

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,24 @@
+locals {
+  ecr_policy_keep_10 = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep the 10 latest images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 10
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "backend_prod" {
   name = "backend-prod"
 }
@@ -16,20 +37,20 @@ resource "aws_ecr_repository" "frontend_staging" {
 
 resource "aws_ecr_lifecycle_policy" "backend_prod" {
   repository = aws_ecr_repository.backend_prod.name
-  policy     = var.ecr_policy_keep_10
+  policy     = local.ecr_policy_keep_10
 }
 
 resource "aws_ecr_lifecycle_policy" "backend_staging" {
   repository = aws_ecr_repository.backend_staging.name
-  policy     = var.ecr_policy_keep_10
+  policy     = local.ecr_policy_keep_10
 }
 
 resource "aws_ecr_lifecycle_policy" "frontend_prod" {
   repository = aws_ecr_repository.frontend_prod.name
-  policy     = var.ecr_policy_keep_10
+  policy     = local.ecr_policy_keep_10
 }
 
 resource "aws_ecr_lifecycle_policy" "frontend_staging" {
   repository = aws_ecr_repository.frontend_staging.name
-  policy     = var.ecr_policy_keep_10
+  policy     = local.ecr_policy_keep_10
 }
--- main.tf
+++ main.tf
@@ -22,25 +22,3 @@ variable "prod_postgres_username" {}
 variable "prod_postgres_password" {}
 variable "staging_postgres_username" {}
 variable "staging_postgres_password" {}
-
-variable "ecr_policy_keep_10" {
-  type    = string
-  default = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Only keep the 10 latest images",
-      "selection": {
-        "tagStatus": "any",
-        "countType": "imageCountMoreThan",
-        "countNumber": 10
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-}
-EOF
-}
