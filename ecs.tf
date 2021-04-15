9be0f51c28912d5ca33af42ac32851f02acbce32
Eero Ruohola <eero@skoleapp.com>
2021-04-15 19:12:15 +0300

    Move JSON container definitions into Terraform files

--- ecs.tf
+++ ecs.tf
@@ -1,21 +1,3 @@
-data "template_file" "container_definitions_prod" {
-  template = file("container-definitions-prod.json")
-
-  vars = {
-    BACKEND_PROD_ECR  = replace(aws_ecr_repository.backend_prod.repository_url, "https://", "")
-    FRONTEND_PROD_ECR = replace(aws_ecr_repository.frontend_prod.repository_url, "https://", "")
-  }
-}
-
-data "template_file" "container_definitions_staging" {
-  template = file("container-definitions-staging.json")
-
-  vars = {
-    BACKEND_STAGING_ECR  = replace(aws_ecr_repository.backend_staging.repository_url, "https://", "")
-    FRONTEND_STAGING_ECR = replace(aws_ecr_repository.frontend_staging.repository_url, "https://", "")
-  }
-}
-
 resource "aws_ecs_cluster" "prod" {
   name = "skole-prod-cluster"
 }
@@ -69,13 +51,213 @@ resource "aws_ecs_service" "staging" {
 resource "aws_ecs_task_definition" "prod" {
   family = "skole-prod-task"
   # TODO make this role in this config.
-  execution_role_arn    = "arn:aws:iam::630869177434:role/skole-ecs-execution-role"
-  container_definitions = data.template_file.container_definitions_prod.rendered
+  execution_role_arn = "arn:aws:iam::630869177434:role/skole-ecs-execution-role"
+  container_definitions = jsonencode(
+    [
+      {
+        name : "backend_prod",
+        image : "${replace(aws_ecr_repository.backend_prod.repository_url, "https://", "")}:latest",
+        cpu : 170,
+        memoryReservation : 332,
+        portMappings : [
+          {
+            containerPort : 8000,
+            protocol : "tcp"
+          }
+        ],
+        logConfiguration : {
+          logDriver : "awslogs",
+          options : {
+            "awslogs-group" : "backend-prod-logs",
+            "awslogs-region" : "eu-central-1"
+          }
+        },
+        essential : true,
+        secrets : [
+          {
+            name : "ALLOWED_HOSTS",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/ALLOWED_HOSTS"
+          },
+          {
+            name : "AWS_ACCESS_KEY_ID",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/ACCESS_KEY_ID"
+          },
+          {
+            name : "AWS_REGION",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/REGION"
+          },
+          {
+            name : "AWS_S3_BUCKET_NAME",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_S3_BUCKET_NAME"
+          },
+          {
+            name : "AWS_S3_BUCKET_NAME_STATIC",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_S3_BUCKET_NAME_STATIC"
+          },
+          {
+            name : "AWS_SECRET_ACCESS_KEY",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/SECRET_ACCESS_KEY"
+          },
+          {
+            name : "CLOUDMERSIVE_API_KEY",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/CLOUDMERSIVE_API_KEY"
+          },
+          {
+            name : "CORS_ALLOWED_ORIGIN_REGEXES",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_CORS_ALLOWED_ORIGIN_REGEXES"
+          },
+          {
+            name : "DATABASE_URL",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_DATABASE_URL"
+          },
+          {
+            name : "DJANGO_SETTINGS_MODULE",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/DJANGO_SETTINGS_MODULE"
+          },
+          {
+            name : "EMAIL_ADDRESS",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_EMAIL_ADDRESS"
+          },
+          {
+            name : "EMAIL_CONTACT_FORM_SENDER",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_EMAIL_CONTACT_FORM_SENDER"
+          },
+          {
+            name : "FCM_SERVER_KEY",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/FCM_SERVER_KEY"
+          },
+          {
+            name : "SECRET_KEY",
+            valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/PROD_SECRET_KEY"
+          }
+        ]
+      },
+      {
+        name : "frontend_prod",
+        image : "${replace(aws_ecr_repository.frontend_prod.repository_url, "https://", "")}:latest",
+        cpu : 341,
+        memoryReservation : 663,
+        portMappings : [
+          {
+            containerPort : 3001,
+            protocol : "tcp"
+          }
+        ],
+        logConfiguration : {
+          logDriver : "awslogs",
+          options : {
+            "awslogs-group" : "frontend-prod-logs",
+            "awslogs-region" : "eu-central-1"
+          }
+        },
+        essential : true
+      }
+    ]
+  )
 }
 
 resource "aws_ecs_task_definition" "staging" {
   family = "skole-staging-task"
   # TODO use the made role here.
-  execution_role_arn    = "arn:aws:iam::630869177434:role/skole-ecs-execution-role"
-  container_definitions = data.template_file.container_definitions_staging.rendered
+  execution_role_arn = "arn:aws:iam::630869177434:role/skole-ecs-execution-role"
+  container_definitions = jsonencode([
+    {
+      name : "backend_staging",
+      image : "${replace(aws_ecr_repository.backend_staging.repository_url, "https://", "")}:latest",
+      cpu : 170,
+      memoryReservation : 332,
+      portMappings : [
+        {
+          containerPort : 8000,
+          protocol : "tcp"
+        }
+      ],
+      logConfiguration : {
+        logDriver : "awslogs",
+        options : {
+          "awslogs-group" : "backend-staging-logs",
+          "awslogs-region" : "eu-central-1"
+        }
+      },
+      essential : true,
+      secrets : [
+        {
+          name : "ALLOWED_HOSTS",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/ALLOWED_HOSTS"
+        },
+        {
+          name : "AWS_ACCESS_KEY_ID",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/ACCESS_KEY_ID"
+        },
+        {
+          name : "AWS_REGION",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/REGION"
+        },
+        {
+          name : "AWS_S3_BUCKET_NAME",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_S3_BUCKET_NAME"
+        },
+        {
+          name : "AWS_S3_BUCKET_NAME_STATIC",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_S3_BUCKET_NAME_STATIC"
+        },
+        {
+          name : "AWS_SECRET_ACCESS_KEY",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/SECRET_ACCESS_KEY"
+        },
+        {
+          name : "CLOUDMERSIVE_API_KEY",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/CLOUDMERSIVE_API_KEY"
+        },
+        {
+          name : "CORS_ALLOWED_ORIGIN_REGEXES",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_CORS_ALLOWED_ORIGIN_REGEXES"
+        },
+        {
+          name : "DATABASE_URL",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_DATABASE_URL"
+        },
+        {
+          name : "DJANGO_SETTINGS_MODULE",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/DJANGO_SETTINGS_MODULE"
+        },
+        {
+          name : "EMAIL_ADDRESS",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_EMAIL_ADDRESS"
+        },
+        {
+          name : "EMAIL_CONTACT_FORM_SENDER",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_EMAIL_CONTACT_FORM_SENDER"
+        },
+        {
+          name : "FCM_SERVER_KEY",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/FCM_SERVER_KEY"
+        },
+        {
+          name : "SECRET_KEY",
+          valueFrom : "arn:aws:ssm:eu-central-1:630869177434:parameter/STAGING_SECRET_KEY"
+        }
+      ]
+    },
+    {
+      name : "frontend_staging",
+      image : "${replace(aws_ecr_repository.frontend_staging.repository_url, "https://", "")}:latest",
+      cpu : 341,
+      memoryReservation : 663,
+      portMappings : [
+        {
+          containerPort : 3001,
+          protocol : "tcp"
+        }
+      ],
+      logConfiguration : {
+        logDriver : "awslogs",
+        options : {
+          "awslogs-group" : "frontend-staging-logs",
+          "awslogs-region" : "eu-central-1"
+        }
+      },
+      essential : true
+    }
+  ])
 }
